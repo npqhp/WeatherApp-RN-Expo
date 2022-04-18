36933126adbfd95f479364ce32441677dd65efd6
phuquoc <npqit4@gmail.com>
2022-04-19 00:18:25 +0700

    Part1

--- App.js
+++ App.js
@@ -1,48 +1,57 @@
-import { 
-  StyleSheet, 
-  Text, 
-  View, 
-  Button,
-  ScrollView,
-  ImageBackground,
-  useWindowDimensions,
-  StatusBar,
-} from 'react-native'
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState}from 'react';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import * as Location from 'expo-location';
 
-const App = () => {
-  const {width: windowWidth, height: windowHeight} = useWindowDimensions();
+import DateTime from './components/DateTime'
+import WeatherScroll from './components/WeatherScroll'
+const API_KEY ='49cc8c821cd2aff9af04c9f98c36eb74';
+
+const img = require('./assets/background.jpg')
+export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        fetchDataFromApi("0", "0")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+      // console.log(data)
+      setData(data)
+      })
+    }
+    
+  }
+
   return (
-    <>
-      <StatusBar barStyle='light-content'/>
-      <ScrollView
-        horizontal={true}
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-      >
-        <View style={{width: windowWidth, height: windowHeight}}>
-          <ImageBackground 
-            source={require('./assets/night2.jpg')}
-            style={{flex: 1}}
-          />
-        </View>
-        <View style={{width: windowWidth, height: windowHeight}}>
-          <ImageBackground 
-            source={require('./assets/rainy.jpg')}
-            style={{flex: 1}}
-          />
-        </View>
-      </ScrollView>
-    </>
-  )
+    <View style={styles.container}>
+      <ImageBackground source={img} style={styles.image} >
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        <WeatherScroll weatherData={data.daily}/>
+      </ImageBackground>
+    </View>
+  );
 }
-
-export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+  },
+  image:{
+    flex:1, 
+    resizeMode:"cover", 
+    justifyContent:"center"
   }
-})
+});
